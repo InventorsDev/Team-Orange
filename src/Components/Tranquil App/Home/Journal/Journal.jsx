3997bb7865e136e5ad1439bcbd8e85da0f7f36a4
DAMDAVE <121645121+DAMDAVE@users.noreply.github.com>
2023-08-13 03:12:27 +0100

    Update Journal.jsx

--- src/Components/Tranquil App/Home/Journal/Journal.jsx
+++ src/Components/Tranquil App/Home/Journal/Journal.jsx
@@ -1,15 +1,42 @@
 import "./Journal.css"; //This is the css file linked to the journal page, any style you define here would affect the html in the return statement
 
 function Journal() {
-     return (
-          //take this space as your normal html, you can start coding from here
-          //all your image imports from figma should be made under Tranquil App/Assets
-          //They should be svg format so as to optimize speed
-          //You can check the other files and compare if you're looking for something but do not edit them
-          <div>
-               <p>Journal Section</p>
-          </div>
-     );
+    return (
+        //take this space as your normal html, you can start coding from here
+        //all your image imports from figma should be made under Tranquil App/Assets
+        //They should be svg format so as to optimize speed
+        //You can check the other files and compare if you're looking for something but do not edit them
+        <div className="journalContainer">
+            <header>
+                <h1>Journal</h1>
+            </header>
+
+            <div className="aboutJournal">
+                <p>Journal is a well recommended act of putting down </p>
+                <p>your thoughts and feelings. Allowing you fully access</p>
+                <p>the many joys in yourself</p>
+            </div>
+
+            <button id="begin">Begin</button>
+
+            <div className="blankPage">
+                <h2>Blank Page</h2>
+                <p>start journaling with a </p>
+                <p>blank Page</p>
+                <button> Empty Page</button>
+            </div>
+
+            <div className="tips">
+                <h2> Tips on what to write about</h2>
+                <div className="ideas">Productivity</div>
+                <div className="ideas">stress & Anxiety</div>
+                <div className="ideas">Therapy</div>
+                <div className="ideas">Happiness</div>
+                <div className="ideas">Self - Discovery</div>
+                <div className="ideas">Relationship</div>
+            </div>
+        </div>
+    );
 }
 
 export default Journal;
